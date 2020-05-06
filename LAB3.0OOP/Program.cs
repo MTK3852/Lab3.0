9053cd2a36d80b09bb4eb4c66fe7d5820e7ef71f
MTK3852 <mtk835123@gmail.com>
2020-05-06 17:15:35 +0300

    Third Lab OOP

---
+++ LAB3.0OOP/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LAB3._0OOP
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+           
+                Console.WriteLine("Enter Line:");
+                string line = Console.ReadLine();
+                string reverseline = ReverseString(line);
+                char[] amount = line.ToCharArray();
+                int number = Number(amount);
+                
+                Console.WriteLine("ReversedLine: "+reverseline);
+                Console.WriteLine("Amount of digits in line: "+number);
+
+                Console.ReadKey();
+            
+           
+
+        }
+        public static string ReverseString(string line)
+        {
+            string reverseline="";
+            for(int i=line.Length-1; i>=0;i--)
+            {
+                reverseline += line[i];
+            }
+            return reverseline;
+        }
+        public static int Number(char[]array)
+        {
+            int number=0;
+           
+            for(int i=0; i<array.Length;i++)
+            {
+                if(char.IsDigit(array[i]))
+                {
+                    number++;
+                }
+            }
+
+            return number;
+        }
+    }
+}
